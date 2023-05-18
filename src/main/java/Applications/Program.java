8eedee36324b29cf24b3a098e83a55c431331f14
FernandoBrambilla <fernandobrambillab1@hotmail.com>
2023-05-18 15:03:37 -0300

    seller entitie class

--- src/main/java/Applications/Program.java
+++ src/main/java/Applications/Program.java
@@ -2,13 +2,15 @@
 package Applications;
 
 import Model.entities.Department;
-import java.sql.Connection;
-import java.sql.SQLException;
+import Model.entities.Seller;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args){
         Department dep = new Department(1, "books");
         System.out.println(dep);
+        Seller seller = new Seller(21, "jose", "jose@email", new Date(), 4000.0, dep);
+        System.out.println(seller);
 }
 }
